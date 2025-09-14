2b06c62b0a95dab0ed087e0b3764edb1a22f7753
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-14 18:56:05 +0300

    Add Phone model class
    Introduces the Phone class with properties for phone number, type, and description, including validation and an EditPhone method for updating details.

---
+++ Contact_Manager_Application/Models/Phone.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_Manager_Application.Models;
+
+internal class Phone
+{
+    private string _phoneNumber = string.Empty;
+    private string _type = string.Empty;
+    private string _description = string.Empty;
+
+    public string PhoneNumber
+    {
+        get => this._phoneNumber;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException(nameof(value));
+            this._phoneNumber = value;
+        }
+    }
+
+    public string Type
+    {
+        get => this._type;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException(nameof(value));
+            this._type = value;
+        }
+    }
+
+    public string Description
+    {
+        get => this._description;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException(nameof(value));
+            this._description = value;
+        }
+    }
+
+    public Phone(string phoneNumber, string type, string description)
+    {
+        this._phoneNumber = phoneNumber;
+        this._type = type;
+        this._description = description;
+    }
+
+    public void EditPhone(Phone phone)
+    {
+        this.PhoneNumber = phone.PhoneNumber;
+        this.Type = phone.Type;
+        this.Description = phone.Description;
+    }
+
+
+}
