b437b018fb9671659514c37493d2d0a12597cf9f
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-15 12:02:22 +0300

    Add main menu UI for contact manager
    Introduces a Menu class with a static method to display the main menu options for the Contact Manager Application. This provides users with options to add, update, delete, and display users, as well as exit the application.

---
+++ Contact_Manager_Application/UI/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_Manager_Application.UI;
+
+internal class Menu
+{
+    public static void ShowMainMenu()
+    {
+        Console.WriteLine("===================================================");
+        Console.WriteLine("              Welcome to Our Application           ");
+        Console.WriteLine("===================================================");
+        Console.WriteLine("  [1]  Add User");
+        Console.WriteLine("  [2]  Update User");
+        Console.WriteLine("  [3]  Delete User");
+        Console.WriteLine("  [4]  Display User");
+        Console.WriteLine("  [5]  Display All Users");
+        Console.WriteLine("  [6]  Exit");
+        Console.WriteLine("===================================================");
+    }
+}
