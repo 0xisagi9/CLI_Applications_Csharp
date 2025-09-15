1dd5126aee64c628a061c2b4916f79391e6527c2
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-15 12:02:38 +0300

    Add UserDataFactory utility for user input helpers
    Introduces the UserDataFactory class with static helper methods to create Address, Phone, and EmailAddress objects by prompting user input. This centralizes and streamlines user data collection in the application.

---
+++ Contact_Manager_Application/Utilities/UserDataFactory.cs
@@ -0,0 +1,44 @@
+﻿using Contact_Manager_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_Manager_Application.Utilities;
+
+internal class UserDataFactory
+{
+
+    public static Address AddressHelper()
+    {
+        return new Address
+        {
+            HomeAddress = InputUtility.ReadInput("Enter Home Address: "),
+            Type = InputUtility.ReadInput("Enter Address Type: "),
+            Description = InputUtility.ReadInput("Enter Address Description: ")
+
+        };
+    }
+
+    public static Phone PhoneHelper()
+    {
+        return new Phone
+        {
+            PhoneNumber = InputUtility.ReadInput("Enter Phone Number : "),
+            Type = InputUtility.ReadInput("Enter Phone Type: "),
+            Description = InputUtility.ReadInput("Enter Phone Description: ")
+        };
+
+    }
+
+    public static EmailAddress EmailHepler()
+    {
+        return new EmailAddress
+        {
+            Email = InputUtility.ReadInput("Enter Email Address: "),
+            Type = InputUtility.ReadInput("Enter Email Type: "),
+            Description = InputUtility.ReadInput("Enter Email Description: ")
+        };
+    }
+}
