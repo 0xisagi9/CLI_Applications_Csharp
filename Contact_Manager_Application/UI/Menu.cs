86a2a0145843094611f140e8d06032ba4a4339ec
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-15 13:14:11 +0300

    Add ShowUpdateMenu method to Menu class
    Introduced a new static method ShowUpdateMenu in the Menu class to display options for updating user information. The menu dynamically shows current user details for each updatable field.

--- Contact_Manager_Application/UI/Menu.cs
+++ Contact_Manager_Application/UI/Menu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contact_Manager_Application.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,24 @@ internal class Menu
         Console.WriteLine("  [4]  Display User");
         Console.WriteLine("  [5]  Display All Users");
         Console.WriteLine("  [6]  Exit");
+        Console.WriteLine("===================================================");
+    }
+
+    public static void ShowUpdateMenu(User user)
+    {
+        Console.Clear();
+        Console.WriteLine("===================================================");
+        Console.WriteLine("              Update User Information              ");
+        Console.WriteLine("===================================================");
+
+        Console.WriteLine($"  [1]  Update First Name ({user.Firstname})");
+        Console.WriteLine($"  [2]  Update  Last Name ({user.Lastname})");
+        Console.WriteLine($"  [3]  Update Gender ({user.Gender})");
+        Console.WriteLine($"  [4]  Update City ({user.City})");
+        Console.WriteLine($"  [5]  Add Email");
+        Console.WriteLine($"  [6]  Add Phone");
+        Console.WriteLine($"  [7]  Add Address");
+        Console.WriteLine($"  [8]  Exit");
         Console.WriteLine("===================================================");
     }
 }
