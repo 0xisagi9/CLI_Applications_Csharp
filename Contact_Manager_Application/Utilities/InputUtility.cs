a32232883d58ab69a135fb498f5d64b185bc1ee5
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-15 12:02:31 +0300

    Add InputUtility helper for reading user input
    Introduces the InputUtility class with a static method to prompt and read user input from the console. This utility will help standardize input handling across the application.

---
+++ Contact_Manager_Application/Utilities/InputUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_Manager_Application.Utilities;
+
+internal class InputUtility
+{
+    public static string ReadInput(string prompt)
+    {
+        Console.WriteLine(prompt);
+        return Console.ReadLine()!;
+    }
+}
