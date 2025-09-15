8928bac2c6b442100d27f938c90bd624e6826b03
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-15 12:02:00 +0300

    Refactor Program.cs to use UI and utility classes
    Moved menu display and user input logic to separate UI and utility classes. Simplified main program flow by delegating user creation to ContanctManager.BuildUser and menu display to Menu.ShowMainMenu, improving code organization and maintainability.

--- Contact_Manager_Application/Program.cs
+++ Contact_Manager_Application/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Contact_Manager_Application.Models;
 using Contact_Manager_Application.Services;
+using Contact_Manager_Application.Utilities;
+using Contact_Manager_Application.UI;
 using System.Runtime.Serialization;
 
 namespace Contact_Manager_Application;
@@ -11,19 +13,18 @@ internal class Program
         try
         {
             bool ProgramRun = true;
-            ContanctManager Database = new();
+            ContanctManager Service = new();
 
             while (ProgramRun)
             {
                 Console.Clear();
-                ShowMainMenu();
+                Menu.ShowMainMenu();
                 Console.WriteLine("Enter Your Choice");
                 string? choice = Console.ReadLine();
                 switch (choice)
                 {
                     case "1": // Add User
-                        User newUser = AddUserFunctionality();
-                        Database.AddUser(newUser);
+                        Service.AddUser(ContanctManager.BuildUser());
 
                         break;
                     case "2": // Update User
@@ -36,7 +37,7 @@ internal class Program
                         break;
 
                     case "5": // Display All Users
-                        Database.ShowAllUsers();
+                        Service.ShowAllUsers();
                         break;
                     case "6":
                         ProgramRun = false;
@@ -45,6 +46,7 @@ internal class Program
                     default:
                         Console.WriteLine("\n Invalid choice, please try again.");
                         break;
+
                 }
 
                 if (ProgramRun)
@@ -61,95 +63,8 @@ internal class Program
         }
 
     }
-    static void ShowMainMenu()
-    {
-        Console.WriteLine("===================================================");
-        Console.WriteLine("              Welcome to Our Application           ");
-        Console.WriteLine("===================================================");
-        Console.WriteLine("  [1]  Add User");
-        Console.WriteLine("  [2]  Update User");
-        Console.WriteLine("  [3]  Delete User");
-        Console.WriteLine("  [4]  Display User");
-        Console.WriteLine("  [5]  Display All Users");
-        Console.WriteLine("  [6]  Exit");
-        Console.WriteLine("===================================================");
-    }
 
-    static User AddUserFunctionality()
-    {
-        User newUser = new();
 
-        Console.Clear();
-        Console.WriteLine("===================================================");
-        Console.WriteLine("              Enter User Information               ");
-        Console.WriteLine("===================================================");
-        newUser.Id = int.Parse(ReadInput("Enter Id: "));
-        newUser.Firstname = ReadInput("Enter First Name: ");
-        newUser.Lastname = ReadInput("Enter Last Name: ");
-        newUser.Gender = ReadInput("Enter Gender: ");
-        newUser.City = ReadInput("Enter City: ");
-
-        // Address
-        do
-        {
-            newUser.AddAddress(AddressHelper());
-
-        } while (ReadInput("Add another Address? (Yes/No): ").Equals("Yes", StringComparison.OrdinalIgnoreCase));
-
-        // Phone
-        do
-        {
-            newUser.AddPhone(PhoneHelper());
-
-        } while (ReadInput("Add another Phone? (Yes/No): ").Equals("Yes", StringComparison.OrdinalIgnoreCase));
-
-        // Email
-
-        do
-        {
-            newUser.AddEmail(EmailHepler());
-
-        } while (ReadInput("Add another Email? (Yes/No): ").Equals("Yes", StringComparison.OrdinalIgnoreCase));
-        return newUser;
-    }
-
-    static Address AddressHelper()
-    {
-        return new Address
-        {
-            HomeAddress = ReadInput("Enter Home Address: "),
-            Type = ReadInput("Enter Address Type: "),
-            Description = ReadInput("Enter Address Description: ")
-
-        };
-    }
-
-    static Phone PhoneHelper()
-    {
-        return new Phone
-        {
-            PhoneNumber = ReadInput("Enter Phone Number : "),
-            Type = ReadInput("Enter Phone Type: "),
-            Description = ReadInput("Enter Phone Description: ")
-        };
-
-    }
-
-    static EmailAddress EmailHepler()
-    {
-        return new EmailAddress
-        {
-            Email = ReadInput("Enter Email Address: "),
-            Type = ReadInput("Enter Email Type: "),
-            Description = ReadInput("Enter Email Description: ")
-        };
-    }
-
-    static string ReadInput(string prompt)
-    {
-        Console.WriteLine(prompt);
-        return Console.ReadLine()!;
-    }
 
 
 
