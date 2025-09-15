3cf57a82c911e1f9b8ed9f9cbfd8afbb339e9dfd
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-15 13:14:26 +0300

    Add sample user and improve user feedback
    Added a sample user to the contact manager at startup for demonstration purposes. Enhanced user feedback by displaying success messages after adding and deleting users, and updated user input prompts for update and delete operations.

--- Contact_Manager_Application/Program.cs
+++ Contact_Manager_Application/Program.cs
@@ -10,10 +10,23 @@ internal class Program
 {
     static void Main()
     {
+
+
         try
         {
             bool ProgramRun = true;
             ContanctManager Service = new();
+            var user1 = new User(
+    1,
+    "Ahmed",
+    "Ali",
+    "Male",
+    "Cairo",
+    new Address("Cairo", "Home", "Main Flat"),
+    new Phone("0101234567", "Mobile", "Personal Phone"),
+    new EmailAddress("ahmed.ali@gmail.com", "Personal", "Primary Email")
+);
+            Service.AddUser(user1);
 
             while (ProgramRun)
             {
@@ -25,12 +38,17 @@ internal class Program
                 {
                     case "1": // Add User
                         Service.AddUser(ContanctManager.BuildUser());
-
+                        Console.WriteLine("\n User added successfully!");
                         break;
                     case "2": // Update User
+                        int updId = int.Parse(InputUtility.ReadInput("Enter Id:"));
+                        Service.UdateUser(updId);
                         break;
 
                     case "3": // Delete User
+                        int delId = int.Parse(InputUtility.ReadInput("Enter Id to delete:"));
+                        Service.RemoveUser(delId);
+                        Console.WriteLine("\n Student deleted successfully!");
                         break;
                     case "4": // Display User
 
