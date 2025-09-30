20689173fdef23c7a00bb2f1e9511d19b222c4a9
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-30 11:37:14 +0300

    Add abstract StaffMembers model class
    Introduces the StaffMembers abstract class with properties for ID, name, phone, email, type, and department, including validation in property setters. Also adds an abstract Pay method to be implemented by derived classes.

---
+++ Employee_Management_Application/Models/StaffMembers.cs
@@ -0,0 +1,77 @@
+﻿namespace Employee_Management_Application.Models;
+
+internal abstract class StaffMembers
+{
+    private int _id;
+    private string _name = string.Empty;
+    private string _phone = string.Empty;
+    private string _email = string.Empty;
+    private string _type = string.Empty;
+    private int _departementId;
+    private readonly Departement _departement;
+
+    public int Id
+    {
+        get => this._id;
+        set
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(value, nameof(value));
+            this._id = value;
+        }
+    }
+    public string Name
+    {
+        get => this._name;
+        set
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(value, nameof(value));
+            this._name = value;
+        }
+    }
+    public string Phone
+    {
+        get => this._phone;
+        set
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(value, nameof(value));
+            this._phone = value;
+        }
+    }
+    public string Email
+    {
+        get => this._email;
+        set
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(value, nameof(value));
+            this._email = value;
+        }
+    }
+    public string Type
+    {
+        get => this._type;
+        set
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(value, nameof(value));
+            this._type = value;
+        }
+    }
+    public int DepartementId
+    {
+        get => this._departementId;
+        set { this._departementId = value; }
+    }
+
+    public StaffMembers(int id, string name, string phone, string email, string type, Departement departement)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Phone = phone;
+        this.Email = email;
+        this.Type = type;
+        this.DepartementId = departement.Id;
+        this._departement = departement;
+    }
+
+    public abstract decimal Pay();
+
+}
