5f0ce4a7a5d919a6d2806e6faa941abf99c02963
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-30 11:37:49 +0300

    Add Departement model class
    Introduces the Departement class with properties for Id, Name, and a list of StaffMembers. Includes validation in property setters and a method to add members.

---
+++ Employee_Management_Application/Models/Departement.cs
@@ -0,0 +1,36 @@
+﻿namespace Employee_Management_Application.Models;
+
+internal class Departement
+{
+    private int _id;
+    private string _name = string.Empty;
+    private readonly List<StaffMembers> _members = [];
+
+    public int Id
+    {
+        get => this._id;
+        set
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(value, nameof(value));
+            this._id = value;
+        }
+    }
+    public string Name
+    {
+        get => this._name;
+        set
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(value, nameof(value));
+            this._name = value;
+        }
+    }
+    public IReadOnlyList<StaffMembers> Members => _members.AsReadOnly();
+    public void AddMember(StaffMembers member) => this._members.Add(member);
+
+    public Departement(int id, string name)
+    {
+        this.Id = id;
+        this.Name = name;
+    }
+
+}
