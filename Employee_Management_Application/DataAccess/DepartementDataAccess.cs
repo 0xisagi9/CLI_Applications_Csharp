f0ec321c8181ede2e3d4f04b1a7af0c44955b3ed
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-30 16:33:33 +0300

    Add DepartementDataAccess for department CRUD operations
    Introduces the DepartementDataAccess class to handle CRUD operations for the Department entity using ADO.NET with DataTable and SqlDataAdapter. Includes methods for retrieving, inserting, updating, and deleting departments, and loads the connection string from appsettings.json.

---
+++ Employee_Management_Application/DataAccess/DepartementDataAccess.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Employee_Management_Application.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Microsoft.IdentityModel.Tokens;
+namespace Employee_Management_Application.DataAccess;
+internal class DepartementDataAccess
+{
+    private readonly string _connectionString;
+    public DepartementDataAccess()
+    {
+        IConfiguration config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        this._connectionString = config.GetConnectionString("DefaultConnection")!;
+    }
+
+    public DataTable GetAllDepartment()
+    {
+        DataTable dataTable = new();
+
+        using SqlConnection connection = new(this._connectionString);
+        string Query = "SELECT DepartmentId, DepartmentName FROM Department";
+        SqlDataAdapter adapter = new(Query, connection);
+        adapter.Fill(dataTable);
+        return dataTable;
+    }
+
+    public void InsertDepartment(string name)
+    {
+        //1. Fill Data With Rows From Database
+        DataTable dataTable = new();
+        using SqlConnection connection = new(this._connectionString);
+        string selectQuery = "SELECT DepartmentId, DepartmentName FROM Department";
+        SqlDataAdapter adapter = new(selectQuery, connection);
+        adapter.Fill(dataTable);
+
+        //2. Insert Command
+        string insertQuery = "INSERT INTO [dbo].[Department]([DepartmentName]) VALUES(@name)";
+        SqlCommand command = new(insertQuery, connection);
+        adapter.InsertCommand = command;
+        adapter.InsertCommand.Parameters.Add("@name", SqlDbType.VarChar, 50, "DepartmentName");
+
+        //3. Insert Row
+        DataRow newRow = dataTable.NewRow();
+        newRow["DepartmentName"] = name;
+        dataTable.Rows.Add(newRow);
+
+        adapter.Update(dataTable);
+
+    }
+
+    public void UpdateDepartment(int id, string name)
+    {
+
+        //1. Fill Data With Rows From Database
+        DataTable dataTable = new();
+        using SqlConnection connection = new(this._connectionString);
+        string selectQuery = "SELECT DepartmentId, DepartmentName FROM Department";
+        SqlDataAdapter adapter = new(selectQuery, connection);
+        adapter.Fill(dataTable);
+
+        //2. Update Command
+        string updateQuery = "UPDATE Department SET DepartmentName = @name WHERE DepartmentId = @id";
+        SqlCommand command = new(updateQuery, connection);
+        adapter.UpdateCommand = command;
+        adapter.UpdateCommand.Parameters.Add("@name", SqlDbType.VarChar, 50, "DepartmentName");
+        adapter.UpdateCommand.Parameters.Add("@id", SqlDbType.Int, 0, "DepartmentId");
+
+        //3. Find the Row to Update
+        DataRow[] rows = dataTable.Select($"DepartmentId = {id}");
+        if (rows.Length > 0)
+            rows[0]["DepartmentName"] = name;
+
+        adapter.Update(dataTable);
+    }
+
+    public void DeleteDepartment(int id)
+    {
+        //1. Fill Data Table with Rows From Database
+        DataTable dataTable = new();
+        using SqlConnection connection = new(this._connectionString);
+        string selectQuery = "SELECT DepartmentId, DepartmentName FROM Department";
+        SqlDataAdapter adapter = new(selectQuery, connection);
+        adapter.Fill(dataTable);
+        //2. Delete Command
+        string deleteQuery = "DELETE FROM Department WHERE DepartmentId= @id";
+        SqlCommand command = new(deleteQuery, connection);
+        adapter.DeleteCommand = command;
+        adapter.DeleteCommand.Parameters.Add("@id", SqlDbType.Int, 0, "DepartmentId");
+
+        //3. Find the Row To Delete
+        DataRow[] rows = dataTable.Select($"DepartmentId = {id}");
+        if (!rows.IsNullOrEmpty())
+            rows[0].Delete();
+
+        adapter.Update(dataTable);
+
+
+    }
+
+    /// Using DataReader
+    //public List<Departement> GetAllDepartement()
+    //{
+    //    var list = new List<Departement>();
+    //    using SqlConnection Connection = new(this._connectionString);
+    //    Connection.Open();
+    //    string Query = "SELECT DepartmentId, DepartmentName FROM Department";
+    //    using SqlCommand command = new(Query, Connection);
+    //    using SqlDataReader reader = command.ExecuteReader();
+    //    while (reader.Read())
+    //    {
+    //        list.Add(new Departement(reader.GetInt32(0), reader.GetString(1)));
+
+    //    }
+    //    Connection.Close();
+
+    //    return list;
+    //}
+
+
+}
