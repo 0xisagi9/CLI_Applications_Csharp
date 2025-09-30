63a2f2943af4bda9cb420306aa955d3f4cf0272d
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-30 11:37:45 +0300

    Add Employees model class
    Introduces the Employees class inheriting from StaffMembers, including an SSN property with null checking and a constructor. The Pay method is stubbed with a NotImplementedException.

---
+++ Employee_Management_Application/Models/Employees.cs
@@ -0,0 +1,27 @@
+﻿namespace Employee_Management_Application.Models;
+
+internal class Employees : StaffMembers
+{
+    private string _ssn = string.Empty;
+    public string SSN
+    {
+        get => this._ssn;
+        set
+        {
+            ArgumentNullException.ThrowIfNull(value, nameof(value));
+            this._ssn = value;
+        }
+    }
+
+    public override decimal Pay()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employees(int id, string name, string phone, string email, string type, Departement departement, string ssn)
+        : base(id, name, phone, email, type, departement)
+    {
+        this.SSN = ssn;
+    }
+
+}
