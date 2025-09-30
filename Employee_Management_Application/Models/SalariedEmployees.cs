787c835dbf5fe1b314689e62b921189f394227dc
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-30 11:37:19 +0300

    Add SalariedEmployees model class
    Introduces the SalariedEmployees class inheriting from Employees, with a Salary property and constructor. The Pay method is stubbed for future implementation.

---
+++ Employee_Management_Application/Models/SalariedEmployees.cs
@@ -0,0 +1,22 @@
+﻿namespace Employee_Management_Application.Models;
+
+internal class SalariedEmployees : Employees
+{
+    private decimal _salary;
+    public decimal Salary
+    {
+        get => this._salary;
+        set => this._salary = value;
+    }
+
+    public SalariedEmployees(int id, string name, string phone, string email, string type, Departement departement, string ssn, decimal slary)
+        : base(id, name, phone, email, type, departement, ssn)
+    {
+        this.Salary = slary;
+    }
+
+    public override decimal Pay()
+    {
+        throw new NotImplementedException();
+    }
+}
