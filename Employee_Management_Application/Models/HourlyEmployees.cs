3ac09fef5c868286e443ecfdf09cf169cfbdfc3c
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-30 11:37:29 +0300

    Add HourlyEmployees model class
    Introduces the HourlyEmployees class inheriting from Employees, with properties for rate and hours, and a constructor to initialize these values. The Pay method is defined but not yet implemented.

---
+++ Employee_Management_Application/Models/HourlyEmployees.cs
@@ -0,0 +1,30 @@
+﻿namespace Employee_Management_Application.Models;
+
+internal class HourlyEmployees : Employees
+{
+    private decimal _rate;
+    private int _hours;
+
+    public decimal Rate
+    {
+        get => this._rate;
+        set => this._rate = value;
+    }
+    public int Hours
+    {
+        get => this._hours;
+        set => this._hours = value;
+    }
+
+    public HourlyEmployees(int id, string name, string phone, string email, string type, Departement departement, string ssn, decimal rate, int hours)
+        : base(id, name, phone, email, type, departement, ssn)
+    {
+        this.Hours = hours;
+        this.Rate = rate;
+    }
+
+    public override decimal Pay()
+    {
+        throw new NotImplementedException();
+    }
+}
