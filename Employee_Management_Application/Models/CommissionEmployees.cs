23ab11fe1d250e6fbd63f3a74378eaeacb32b3a2
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-30 11:36:59 +0300

    Add CommissionEmployees model class
    Introduces the CommissionEmployees class, inheriting from Employees, with a Target property and a constructor. The Pay method is defined but not yet implemented.

---
+++ Employee_Management_Application/Models/CommissionEmployees.cs
@@ -0,0 +1,22 @@
+﻿namespace Employee_Management_Application.Models;
+
+internal class CommissionEmployees : Employees
+{
+    private decimal _target;
+    public decimal Target
+    {
+        get => this._target;
+        set => this._target = value;
+    }
+
+    public CommissionEmployees(int id, string name, string phone, string email, string type, Departement departement, string ssn, decimal target)
+        : base(id, name, phone, email, type, departement, ssn)
+    {
+        this.Target = target;
+    }
+
+    public override decimal Pay()
+    {
+        throw new NotImplementedException();
+    }
+}
