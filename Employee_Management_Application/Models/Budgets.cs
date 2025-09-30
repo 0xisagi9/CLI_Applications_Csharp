b16f9cf9955b787c6b9ea9b3f90c072cf51e6e42
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-30 11:36:55 +0300

    Add Budgets model class
    Introduces a new Budgets class with Id and Value properties to represent budget data in the application.

---
+++ Employee_Management_Application/Models/Budgets.cs
@@ -0,0 +1,25 @@
+﻿namespace Employee_Management_Application.Models;
+
+internal class Budgets
+{
+    private int _id;
+    private decimal _value;
+
+    public int Id
+    {
+        get => this._id;
+        set => this._id = value;
+    }
+
+    public decimal Value
+    {
+        get => this._value;
+        set => this._value = value;
+    }
+
+    public Budgets(int id, decimal value)
+    {
+        this.Id = id;
+        this.Value = value;
+    }
+}
