0f6077534344bf29cf2d75f747b31ca6713ed08f
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-30 11:37:33 +0300

    Add ExecutiveEmployees model class
    Introduces the ExecutiveEmployees class inheriting from Employees, with properties for Salary and Bonus, and a constructor to initialize these fields. The Pay method is stubbed for future implementation.

---
+++ Employee_Management_Application/Models/ExecutiveEmployees.cs
@@ -0,0 +1,31 @@
+﻿namespace Employee_Management_Application.Models;
+
+internal class ExecutiveEmployees : Employees
+{
+    private decimal _salary;
+    private decimal _bonus;
+
+    public decimal Salary
+    {
+        get => this._salary;
+        set => this._salary = value;
+    }
+    public decimal Bonus
+    {
+        get => this._bonus;
+        set => this._bonus = value;
+    }
+
+
+    public ExecutiveEmployees(int id, string name, string phone, string email, string type, Departement departement, string ssn, decimal salary, decimal bonus)
+        : base(id, name, phone, email, type, departement, ssn)
+    {
+        this.Salary = salary;
+        this.Bonus = bonus;
+    }
+
+    public override decimal Pay()
+    {
+        throw new NotImplementedException();
+    }
+}
