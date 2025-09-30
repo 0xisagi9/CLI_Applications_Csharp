587942d96f5521554eb43aa15dae8607f6297342
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-30 11:37:04 +0300

    Add Volunteers model class
    Introduces the Volunteers class inheriting from StaffMembers, with a Value property and a constructor. The Pay method is defined but not yet implemented.

---
+++ Employee_Management_Application/Models/Volunteers.cs
@@ -0,0 +1,22 @@
+﻿namespace Employee_Management_Application.Models;
+
+internal class Volunteers : StaffMembers
+{
+    private decimal _value;
+    public decimal Value
+    {
+        get => this._value;
+        set => this._value = value;
+    }
+
+    public Volunteers(int id, string name, string phone, string email, string type, Departement departement, decimal value)
+        : base(id, name, phone, email, type, departement)
+    {
+        this.Value = value;
+    }
+
+    public override decimal Pay()
+    {
+        throw new NotImplementedException();
+    }
+}
