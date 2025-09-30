ed5ea382bc3d66d85a04bb429c2bce7d668804ee
0xisagi9 <muhammed9alii9@gmail.com>
2025-09-30 11:37:25 +0300

    Add Projects model class
    Introduces the Projects class to represent project entities, including properties for ID, location, current cost, manager, and associated budgets. Provides methods for managing budgets and enforces validation on the location property.

---
+++ Employee_Management_Application/Models/Projects.cs
@@ -0,0 +1,42 @@
+﻿namespace Employee_Management_Application.Models;
+
+internal class Projects
+{
+    private int _id;
+    private string _location = string.Empty;
+    private decimal _currentCost;
+    private readonly int _managerId;
+    private readonly Employees _manager;
+    private readonly List<Budgets> _budgets = [];
+
+    public int Id
+    {
+        get => this._id; set => this._id = value;
+    }
+    public string Location
+    {
+        get => this._location;
+        set
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(value, nameof(value));
+            this._location = value;
+        }
+    }
+    public decimal CurrentCost
+    {
+        get => this._currentCost;
+        set => this._currentCost = value;
+    }
+
+    public IReadOnlyList<Budgets> Budgets => this._budgets.AsReadOnly();
+
+    public Projects(int id, string location, decimal currentCost, Employees manager)
+    {
+        this.Id = id;
+        this.Location = location;
+        this.CurrentCost = currentCost;
+        this._managerId = manager.Id;
+        this._manager = manager;
+    }
+    public void AddBudget(Budgets budget) => this._budgets.Add(budget);
+}
