7880dff64c23ef09cb235a5babef6ff2dff356ac
0xisagi9 <muhammed9alii9@gmail.com>
2025-10-04 15:47:34 +0300

    Add SQL support to CommissionEmployees model
    Updated CommissionEmployees to use departementId instead of Departement, added constructors, and implemented SQL procedure integration with AddParameters and ProcedureName for database operations.

--- Employee_Management_Application/Models/CommissionEmployees.cs
+++ Employee_Management_Application/Models/CommissionEmployees.cs
@@ -1,4 +1,6 @@
-﻿namespace Employee_Management_Application.Models;
+﻿using Microsoft.Data.SqlClient;
+
+namespace Employee_Management_Application.Models;
 
 internal class CommissionEmployees : Employees
 {
@@ -9,12 +11,26 @@ internal class CommissionEmployees : Employees
         set => this._target = value;
     }
 
-    public CommissionEmployees(int id, string name, string phone, string email, string type, Departement departement, string ssn, decimal target)
-        : base(id, name, phone, email, type, departement, ssn)
+    public CommissionEmployees(int id, string name, string phone, string email, string type, int departementId, string ssn, decimal target)
+        : base(id, name, phone, email, type, departementId, ssn)
+    {
+        this.Target = target;
+    }
+    public CommissionEmployees(string name, string phone, string email, string type, int departementId, string ssn, decimal target)
+        : base(name, phone, email, type, departementId, ssn)
     {
         this.Target = target;
     }
 
+    public CommissionEmployees() { }
+
+    public override string ProcedureName => "AddCommissionEmployee";
+
+    public override void AddParameters(SqlCommand command)
+    {
+        base.AddParameters(command);
+        command.Parameters.AddWithValue("@Target", this.Target);
+    }
     public override decimal Pay()
     {
         throw new NotImplementedException();
