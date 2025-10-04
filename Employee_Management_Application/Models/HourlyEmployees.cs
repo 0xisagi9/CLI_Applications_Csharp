b9b6ef5e8e249e6b037ef994034972048bb0db5d
0xisagi9 <muhammed9alii9@gmail.com>
2025-10-04 15:47:29 +0300

    Add SQL integration to HourlyEmployees model
    Introduced SQL command parameterization for HourlyEmployees by overriding AddParameters and specifying ProcedureName. Updated constructors to use departementId instead of Departement object, and added a parameterless constructor for flexibility.

--- Employee_Management_Application/Models/HourlyEmployees.cs
+++ Employee_Management_Application/Models/HourlyEmployees.cs
@@ -1,4 +1,6 @@
-﻿namespace Employee_Management_Application.Models;
+﻿using Microsoft.Data.SqlClient;
+
+namespace Employee_Management_Application.Models;
 
 internal class HourlyEmployees : Employees
 {
@@ -16,11 +18,30 @@ internal class HourlyEmployees : Employees
         set => this._hours = value;
     }
 
-    public HourlyEmployees(int id, string name, string phone, string email, string type, Departement departement, string ssn, decimal rate, int hours)
-        : base(id, name, phone, email, type, departement, ssn)
+    public HourlyEmployees(int id, string name, string phone, string email, string type, int departementId, string ssn, decimal rate, int hours)
+        : base(id, name, phone, email, type, departementId, ssn)
     {
         this.Hours = hours;
         this.Rate = rate;
+    }
+    public HourlyEmployees(string name, string phone, string email, string type, int departementId, string ssn, decimal rate, int hours)
+        : base(name, phone, email, type, departementId, ssn)
+    {
+        this.Hours = hours;
+        this.Rate = rate;
+    }
+
+    public HourlyEmployees() { }
+
+
+
+    public override string ProcedureName => "AddHourlyEmployee";
+
+    public override void AddParameters(SqlCommand command)
+    {
+        base.AddParameters(command);
+        command.Parameters.AddWithValue("@Rate", this.Rate);
+        command.Parameters.AddWithValue("@Hours", this.Hours);
     }
 
     public override decimal Pay()
