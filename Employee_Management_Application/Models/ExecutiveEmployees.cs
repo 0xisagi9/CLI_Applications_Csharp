5a03ab136ab863900420e72b539c1a58324cfb36
0xisagi9 <muhammed9alii9@gmail.com>
2025-10-04 15:45:55 +0300

    Add SQL integration to ExecutiveEmployees model
    Introduced SQL command parameterization for ExecutiveEmployees by adding AddParameters method and ProcedureName property. Updated constructors to support department ID instead of department object, improving database integration.

--- Employee_Management_Application/Models/ExecutiveEmployees.cs
+++ Employee_Management_Application/Models/ExecutiveEmployees.cs
@@ -1,4 +1,6 @@
-﻿namespace Employee_Management_Application.Models;
+﻿using Microsoft.Data.SqlClient;
+
+namespace Employee_Management_Application.Models;
 
 internal class ExecutiveEmployees : Employees
 {
@@ -16,14 +18,29 @@ internal class ExecutiveEmployees : Employees
         set => this._bonus = value;
     }
 
-
-    public ExecutiveEmployees(int id, string name, string phone, string email, string type, Departement departement, string ssn, decimal salary, decimal bonus)
-        : base(id, name, phone, email, type, departement, ssn)
+    public ExecutiveEmployees(int id, string name, string phone, string email, string type, int departementId, string ssn, decimal salary, decimal bonus)
+        : base(id, name, phone, email, type, departementId, ssn)
     {
         this.Salary = salary;
         this.Bonus = bonus;
     }
 
+    public ExecutiveEmployees(string name, string phone, string email, string type, int departementId, string ssn, decimal salary, decimal bonus)
+        : base(name, phone, email, type, departementId, ssn)
+    {
+        this.Salary = salary;
+        this.Bonus = bonus;
+    }
+
+    public ExecutiveEmployees() { }
+    public override string ProcedureName => "AddExecutiveEmployee";
+
+    public override void AddParameters(SqlCommand command)
+    {
+        base.AddParameters(command);
+        command.Parameters.AddWithValue("@Salary", this.Salary);
+        command.Parameters.AddWithValue("@Bonus", this.Bonus);
+    }
     public override decimal Pay()
     {
         throw new NotImplementedException();
