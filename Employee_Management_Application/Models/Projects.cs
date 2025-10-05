f19be3c636b5d35c361ba8124537610e7a6562eb
0xisagi9 <muhammed9alii9@gmail.com>
2025-10-05 11:13:25 +0300

    Update Projects.cs

--- Employee_Management_Application/Models/Projects.cs
+++ Employee_Management_Application/Models/Projects.cs
@@ -5,8 +5,7 @@ internal class Projects
     private int _id;
     private string _location = string.Empty;
     private decimal _currentCost;
-    private readonly int _managerId;
-    private readonly Employees _manager;
+    private int _managerId;
     private readonly List<Budgets> _budgets = [];
 
     public int Id
@@ -28,15 +27,32 @@ internal class Projects
         set => this._currentCost = value;
     }
 
+    public int ManagerId
+    {
+        get => this._managerId;
+        set
+        {
+            this._managerId = value;
+        }
+    }
+
     public IReadOnlyList<Budgets> Budgets => this._budgets.AsReadOnly();
 
-    public Projects(int id, string location, decimal currentCost, Employees manager)
+    public Projects(int id, string location, decimal currentCost, int managerId)
     {
         this.Id = id;
         this.Location = location;
         this.CurrentCost = currentCost;
-        this._managerId = manager.Id;
-        this._manager = manager;
+        this.ManagerId = managerId;
     }
+
+    public Projects(string location, decimal currentCost, int managerId)
+    {
+        this.Location = location;
+        this.CurrentCost = currentCost;
+        this.ManagerId = managerId;
+    }
+
+    public Projects() { }
     public void AddBudget(Budgets budget) => this._budgets.Add(budget);
 }
