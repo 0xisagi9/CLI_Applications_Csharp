1ee25e1a3ca457c562f17cb7666f6a48162ee1b5
0xisagi9 <muhammed9alii9@gmail.com>
2025-10-05 11:13:27 +0300

    Update Volunteers.cs

--- Employee_Management_Application/Models/Volunteers.cs
+++ Employee_Management_Application/Models/Volunteers.cs
@@ -1,4 +1,6 @@
-﻿namespace Employee_Management_Application.Models;
+﻿using Microsoft.Data.SqlClient;
+
+namespace Employee_Management_Application.Models;
 
 internal class Volunteers : StaffMembers
 {
@@ -9,14 +11,31 @@ internal class Volunteers : StaffMembers
         set => this._value = value;
     }
 
-    public Volunteers(int id, string name, string phone, string email, string type, Departement departement, decimal value)
-        : base(id, name, phone, email, type, departement)
+    public Volunteers(int id, string name, string phone, string email, string type, int departementId, decimal value)
+        : base(id, name, phone, email, type, departementId)
+    {
+        this.Value = value;
+    }
+    public Volunteers(string name, string phone, string email, string type, int departementId, decimal value)
+        : base(name, phone, email, type, departementId)
     {
         this.Value = value;
     }
 
+    public Volunteers() { }
+
     public override decimal Pay()
     {
         throw new NotImplementedException();
+    }
+
+    public override string ProcedureName => "AddVolunteer";
+    public override void AddParameters(SqlCommand command)
+    {
+        command.Parameters.AddWithValue("@Name", this.Name);
+        command.Parameters.AddWithValue("@Phone", this.Phone);
+        command.Parameters.AddWithValue("@Email", this.Email);
+        command.Parameters.AddWithValue("@DepartmentId", this.DepartementId);
+        command.Parameters.AddWithValue("@Value", this.Value);
     }
 }
