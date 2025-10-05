f63e18f0bc50900b757a29fddcc4b0fe5879fe00
0xisagi9 <muhammed9alii9@gmail.com>
2025-10-05 11:13:30 +0300

    Create Program.cs

---
+++ Employee_Management_Application/Program.cs
@@ -0,0 +1,24 @@
+﻿using Employee_Management_Application.DataAccess;
+using Employee_Management_Application.Models;
+using Employee_Management_Application.UI;
+using System.Data;
+
+namespace Employee_Management_Application;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Menu.MainMenu();
+
+        }
+        catch (Exception ex)
+        {
+            WriteLine(ex.Message);
+        }
+
+    }
+
+}
