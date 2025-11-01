4decb5d5abed1ee69fa63f3dd1273511e849a8a3
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-01 17:02:45 +0200

    Create CreateUsersTable.sql

---
+++ Clinic_Management_System/Database/CreateUsersTable.sql
@@ -0,0 +1,35 @@
+USE [Clinic_Management_System]
+GO
+
+--															Create User Table
+
+CREATE TABLE [dbo].[User]
+(
+[UserId] INT IDENTITY(1,1) NOT NULL,
+[Name] VARCHAR(50) NOT NULL,
+[Role] VARCHAR(50) NOT NULL,
+[Email] VARCHAR(50) NOT NULL,
+[Password] VARCHAR(50) NOT NULL,
+
+CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserId] ASC),
+CONSTRAINT [UQ_User_Email] UNIQUE ([Email])
+)
+
+--															Create Doctor Table
+CREATE TABLE [dbo].[Doctor]
+(
+[DoctorId] INT NOT NULL,
+[Specification] VARCHAR(50) NOT NULL,
+CONSTRAINT [PK_Doctor] PRIMARY KEY CLUSTERED ([DoctorId] ASC),
+CONSTRAINT [FK_Doctor_User] FOREIGN KEY ([DoctorId]) REFERENCES [dbo].[User]([UserId]) ON DELETE CASCADE
+)
+
+--															Create Patient Table
+
+CREATE TABLE [dbo].[Patient]
+(
+[PatientId] INT NOT NULL,
+[MedicalNotes] VARCHAR(50) NOT NULL,
+CONSTRAINT [PK_Patient] PRIMARY KEY CLUSTERED ([PatientId] ASC),
+CONSTRAINT [FK_Patient_User] FOREIGN KEY ([PatientId]) REFERENCES [dbo].[User]([UserId]) ON DELETE CASCADE
+)
