f6c8eb7369356a6279655a255dadd47300a565cd
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-03 22:08:08 +0200

    Create AppointmentSlot.cs

---
+++ Clinic_Management_System/Business/Models/AppointmentSlot.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Clinic_Management_System.Business.Models;
+
+internal class AppointmentSlot
+{
+    public int SlotId { get; set; }
+    public int DoctorId { get; set; }
+    public DateTime? StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
+    public bool IsBooked { get; set; }
+
+    public AppointmentSlot() { }
+
+    public AppointmentSlot(int slotId, int doctorId, DateTime? startTime, DateTime? endTime, bool isBooked)
+    {
+        SlotId = slotId;
+        DoctorId = doctorId;
+        StartTime = startTime;
+        EndTime = endTime;
+        IsBooked = isBooked;
+    }
+
+
+}
