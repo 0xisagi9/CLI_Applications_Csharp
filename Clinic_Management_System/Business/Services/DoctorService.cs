dc432d091cdff70057194679cec6f583d777a01e
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-03 22:08:03 +0200

    Create DoctorService.cs

---
+++ Clinic_Management_System/Business/Services/DoctorService.cs
@@ -0,0 +1,44 @@
+﻿using Clinic_Management_System.Business.Models;
+using Clinic_Management_System.Core.Interfaces;
+using Microsoft.Data.SqlClient;
+using System.Runtime;
+
+namespace Clinic_Management_System.Business.Services;
+
+internal class DoctorService(IRepository<Doctor> doctorRepo)
+{
+    private readonly IRepository<Doctor> _doctorRepo = doctorRepo;
+
+    public IEnumerable<Doctor> GetAllDoctors() => _doctorRepo.GetAll();
+    public void AddNewDoctor(Doctor doctor)
+    {
+        try
+        {
+            _doctorRepo.Add(doctor);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Falied to Add new Doctor, {ex.Message}");
+        }
+    }
+
+    public void DeleteDoctor(int doctorId)
+    {
+        try
+        {
+            _doctorRepo.Delete(doctorId);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Doctor with Id:{doctorId} Not found, {ex.Message}");
+        }
+    }
+
+    public Doctor GetDoctorById(int doctorId) => _doctorRepo.GetById(doctorId);
+
+    public void UpdateDoctr(int doctorId, Doctor doctor)
+    {
+        doctor.UserId = doctorId;
+        _doctorRepo.Update(doctor);
+    }
+}
