0598b5346681d136cec11db06b6a016c8a7e6a88
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-03 22:07:12 +0200

    Create User.cs

---
+++ Clinic_Management_System/Business/Models/User.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System.Runtime;
+
+namespace Clinic_Management_System.Business.Models;
+
+internal class User
+{
+    public int UserId { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? Role { get; set; }
+
+
+    public User() { }
+    public User(int id, string name, string email, string password, string role)
+    {
+        UserId = id;
+        Name = name;
+        Email = email;
+        Password = password;
+        Role = role;
+    }
+    public User(string name, string email, string password, string role)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        Role = role;
+    }
+
+
+    public override string ToString() => $"Id: {UserId}, Name: {Name}, Email:{Email}, Role: {Role}";
+
+
+}
