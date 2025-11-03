114fe1b3dcf71969fb8e744defa92db66b5c06b0
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-03 22:08:13 +0200

    Add AppConfiguration class for config management
    Introduces AppConfiguration to load settings from appsettings.json and provide connection string retrieval with error handling.

---
+++ Clinic_Management_System/Configuration/AppConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+namespace Clinic_Management_System.Configuration;
+
+internal class AppConfiguration
+{
+    private readonly IConfigurationRoot _config;
+
+    public AppConfiguration()
+    {
+        _config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+    }
+
+    public string GetConncetionString(string name = "DefaultConnection")
+    {
+        var connectionString = _config.GetConnectionString(name);
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException($"Connection string '{name}' not found in configuration file.");
+        }
+
+        return connectionString;
+    }
+}
