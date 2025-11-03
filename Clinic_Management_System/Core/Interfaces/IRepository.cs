4e65394a77bf599e665e51226666e56f7a61c989
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-03 22:07:26 +0200

    Add generic IRepository interface
    Introduces a generic IRepository<T> interface for basic CRUD operations, including methods for retrieving, adding, updating, and deleting entities. This provides a foundation for repository pattern implementation in the system.

---
+++ Clinic_Management_System/Core/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Clinic_Management_System.Core.Interfaces;
+
+internal interface IRepository<T>
+{
+    T GetById(int id);
+    IEnumerable<T> GetAll();
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(int id);
+}
+
+
