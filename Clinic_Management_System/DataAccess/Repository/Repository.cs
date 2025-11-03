92a0a7693bfdb1eed65e4ae9094b81f37c6a420f
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-03 22:07:52 +0200

    Add generic repository implementation
    Introduces a generic Repository<T> class using Dapper for basic data access operations. Only the GetAll method is implemented; other CRUD methods throw NotImplementedException.

---
+++ Clinic_Management_System/DataAccess/Repository/Repository.cs
@@ -0,0 +1,34 @@
+﻿using Clinic_Management_System.Core.Interfaces;
+using Dapper;
+using System.Data;
+
+namespace Clinic_Management_System.DataAccess.Repository;
+
+internal class Repository<T>(IDbConnection connection, string tableName) : IRepository<T>
+{
+    protected readonly IDbConnection _connection = connection;
+    protected readonly string _tableName = tableName;
+
+    public virtual IEnumerable<T> GetAll()
+    {
+        return _connection.Query<T>($"SELECT * FROM {_tableName}");
+    }
+
+    public virtual T GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual void Add(T entity)
+    {
+        throw new NotImplementedException();
+    }
+    public virtual void Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+    public virtual void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
