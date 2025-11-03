19aab20104965e34cfb31ef3225283982f6b2f8d
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-03 22:07:31 +0200

    Add SqlConnectionFactory for database connections
    Introduces SqlConnectionFactory implementing ISqlConnectionFactory to manage SQL Server connections. This class provides a method to create and open a new SqlConnection using a provided connection string.

---
+++ Clinic_Management_System/DataAccess/DatabaseConnect/SqlConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using Clinic_Management_System.Core.Interfaces;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+
+namespace Clinic_Management_System.DataAccess.DatabaseConnect;
+
+internal class SqlConnectionFactory(string connectionString) : ISqlConnectionFactory
+{
+    private readonly string _connectionString = connectionString;
+
+    public IDbConnection CreateConnection()
+    {
+        var connection = new SqlConnection(_connectionString);
+        connection.Open();
+        return connection;
+    }
+}
