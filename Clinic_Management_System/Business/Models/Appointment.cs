e3f7b68e8c058c5e1d07b359fb935bbae7a97e4e
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-03 22:07:56 +0200

    Create Appointment.cs

---
+++ Clinic_Management_System/Business/Models/Appointment.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Clinic_Management_System.Business.Models;
+
+internal class Appointment
+{
+    public int AppointmentId { get; set; }
+    public int SlotId { get; set; }
+    public int PatientID { get; set; }
+    public string? Status { get; set; }
+
+    public Appointment() { }
+
+    public Appointment(int appointmentId, int slotId, int patientID, string? status)
+    {
+        AppointmentId = appointmentId;
+        SlotId = slotId;
+        PatientID = patientID;
+        Status = status;
+    }
+}
