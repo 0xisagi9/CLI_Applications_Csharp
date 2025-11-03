640758d15a36da6a447d45b6dcf08896943aafff
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-03 22:07:07 +0200

    Create Doctor.cs

---
+++ Clinic_Management_System/Business/Models/Doctor.cs
@@ -0,0 +1,19 @@
+﻿namespace Clinic_Management_System.Business.Models;
+
+internal class Doctor : User
+{
+    public string? Specification { get; set; }
+
+    public Doctor() : base() { }
+    public Doctor(int id, string name, string email, string password, string role, string specification) : base(id, name, email, password, role)
+    {
+        Specification = specification;
+    }
+    public Doctor(string name, string email, string password, string role, string specification) : base(name, email, password, role)
+    {
+        Specification = specification;
+    }
+
+    public override string ToString() => base.ToString() + $" Specification:{Specification}";
+
+}
