a9320b1dc98c76fffc55606dad7bdc5ecd07986e
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-03 22:07:10 +0200

    Create Patient.cs

---
+++ Clinic_Management_System/Business/Models/Patient.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Runtime;
+
+namespace Clinic_Management_System.Business.Models;
+
+internal class Patient : User
+{
+    public string? MedicalNotes { get; set; }
+
+
+    public Patient() : base() { }
+
+    public Patient(int id, string name, string email, string password, string role, string medicalNotes) : base(id, name, email, password, role)
+    {
+        MedicalNotes = medicalNotes;
+    }
+
+    public Patient(string name, string email, string password, string role, string medicalNotes) : base(name, email, password, role)
+    {
+        MedicalNotes = medicalNotes;
+    }
+
+    public override string ToString() => base.ToString() + $"Medical Notes: {MedicalNotes}";
+}
