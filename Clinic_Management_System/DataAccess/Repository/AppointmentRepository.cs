9e789f069a09d3cdda68130dc67dde85e960c4d9
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-06 03:04:50 +0200

    Add AppointmentRepository for managing appointments
    Introduces AppointmentRepository with methods to book appointments and view appointments by patient ID. Utilizes Dapper for database operations and supports stored procedure execution.

---
+++ Clinic_Management_System/DataAccess/Repository/AppointmentRepository.cs
@@ -0,0 +1,30 @@
+﻿using Clinic_Management_System.Business.Models;
+using Clinic_Management_System.Core.Interfaces;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_System.DataAccess.Repository;
+
+internal class AppointmentRepository(IDbConnection connection) : Repository<Appointment>(connection, "Appointment")
+{
+    public void BookAppointment(Appointment appointment)
+    {
+        var parameters = new DynamicParameters();
+        parameters.Add("@SlotId", appointment.SlotId);
+        parameters.Add("@PatientId", appointment.PatientID);
+
+
+        _connection.Execute("AddAppointment", parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public IEnumerable<Appointment> ViewAppointmentById(int patientId)
+    {
+        var sql = "SELECT * FROM Appointment WHERE PatientId = @PatientId";
+        return _connection.Query<Appointment>(sql, new { PatientId = patientId });
+    }
+}
