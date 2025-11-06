ef89a121e45d69b414168a534083a489ea5bc146
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-06 03:06:02 +0200

    Create AvailableSlotsDTO.cs

---
+++ Clinic_Management_System/DataAccess/DTO/AvailableSlotsDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_System.DataAccess.DTO;
+
+internal class AvailableSlotsDTO
+{
+    public string? Name { get; set; }
+    public int DoctorId { get; set; }
+    public int SlotId { get; set; }
+    public string? Specification { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+}
