d2f962b097e39efbb553721e5ee78e2c77e172ba
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-06 03:03:37 +0200

    Add AuthService for user authentication logic
    Introduces AuthService with methods for user sign-up, login, deactivation, and validation. Integrates with UserRepository to manage user authentication and account status.

---
+++ Clinic_Management_System/Business/Services/AuthService.cs
@@ -0,0 +1,55 @@
+﻿using Clinic_Management_System.Business.Models;
+using Clinic_Management_System.DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_System.Business.Services;
+
+internal class AuthService(UserRepository userRepo)
+{
+    private readonly UserRepository _userRepo = userRepo;
+
+    public void SignUp(User user)
+    {
+        if (_userRepo.ExistByEmail(user.Email!))
+            throw new Exception("Email Already Exist");
+
+        _userRepo.Add(user);
+    }
+
+    public User Login(string email, string password)
+    {
+
+        var user = _userRepo.GetByEmail(email) ?? throw new Exception("User Not Found");
+        if (!user.IsActive)
+            throw new Exception("Account is Deactivated");
+        if (user.Password != password)
+            throw new Exception("Invalid Password");
+
+        return user;
+    }
+
+    public void DeactivateUser(int userId)
+    {
+        var user = _userRepo.GetById(userId);
+        if (user != null)
+            throw new Exception("User Not Found");
+        if (!user.IsActive)
+            throw new Exception("User Already Deactivated");
+
+        _userRepo.Delete(userId);
+    }
+
+
+    private bool IsValidUser(string email, string password)
+    {
+        var user = _userRepo.GetByEmail(email);
+        return user != null && user.Password == password && user.IsActive;
+    }
+
+}
