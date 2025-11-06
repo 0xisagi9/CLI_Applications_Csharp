b3adb14a5c14320e7b66336c1e9be94b1b7b79fd
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-06 03:00:10 +0200

    Add AppointmentService for managing appointments
    Introduces AppointmentService with methods to retrieve all appointments and add a new appointment using the repository pattern. Includes basic exception handling for adding appointments.

---
+++ Clinic_Management_System/Business/Services/AppointmentService.cs
@@ -0,0 +1,31 @@
+﻿using Clinic_Management_System.Business.Models;
+using Clinic_Management_System.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_System.Business.Services
+{
+    internal class AppointmentService(IRepository<Appointment> appointmentRepo)
+    {
+        private readonly IRepository<Appointment> _appointmentRepo = appointmentRepo;
+
+        public IEnumerable<Appointment> GetAllAppointments() => _appointmentRepo.GetAll();
+
+        public void AddNewAppointment(Appointment appointment)
+        {
+            try
+            {
+                _appointmentRepo.Add(appointment);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to Add New Appointment, {ex.Message}");
+            }
+        }
+    }
+}
