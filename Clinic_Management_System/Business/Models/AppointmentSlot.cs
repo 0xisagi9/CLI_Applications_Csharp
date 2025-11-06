2e3eba7aff31b83436cc00b1ed6c0e91841b41dc
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-06 03:06:10 +0200

    Update AppointmentSlot.cs

--- Clinic_Management_System/Business/Models/AppointmentSlot.cs
+++ Clinic_Management_System/Business/Models/AppointmentSlot.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace Clinic_Management_System.Business.Models;
+﻿namespace Clinic_Management_System.Business.Models;
 
 internal class AppointmentSlot
 {
@@ -10,15 +8,36 @@ internal class AppointmentSlot
     public DateTime? EndTime { get; set; }
     public bool IsBooked { get; set; }
 
+    public bool IsActive { get; set; }
+
     public AppointmentSlot() { }
 
-    public AppointmentSlot(int slotId, int doctorId, DateTime? startTime, DateTime? endTime, bool isBooked)
+    public AppointmentSlot(int slotId, int doctorId, DateTime? startTime, DateTime? endTime, bool isBooked, bool isActive)
     {
         SlotId = slotId;
         DoctorId = doctorId;
         StartTime = startTime;
         EndTime = endTime;
         IsBooked = isBooked;
+        IsActive = isActive;
+    }
+
+    public AppointmentSlot(int doctorId, DateTime? startTime, DateTime? endTime)
+    {
+        DoctorId = doctorId;
+        StartTime = startTime;
+        EndTime = endTime;
+
+    }
+
+    public override string ToString()
+    {
+        return $"Slot ID: {SlotId}, " +
+          $"Doctor ID: {DoctorId}, " +
+          $"Start Time: {(StartTime.HasValue ? StartTime.Value.ToString("yyyy-MM-dd HH:mm") : "N/A")}, " +
+          $"End Time: {(EndTime.HasValue ? EndTime.Value.ToString("yyyy-MM-dd HH:mm") : "N/A")}, " +
+          $"Is Booked: {(IsBooked ? "Yes" : "No")}," +
+          $"Is Active: {(IsActive ? "Yes" : "No")}";
     }
 
 
