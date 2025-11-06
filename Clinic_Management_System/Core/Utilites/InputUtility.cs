dd89a11ae10480666755d5877d9e16bccbfb4ade
0xisagi9 <muhammed9alii9@gmail.com>
2025-11-06 03:06:23 +0200

    Create InputUtility.cs

---
+++ Clinic_Management_System/Core/Utilites/InputUtility.cs
@@ -0,0 +1,10 @@
+﻿namespace Clinic_Management_System.Core.Utilites;
+
+internal class InputUtility
+{
+    public static string ReadInput(string prompt)
+    {
+        Console.Write(prompt);
+        return Console.ReadLine()!;
+    }
+}
